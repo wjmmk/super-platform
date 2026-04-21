e690864023b4b3819b3fba62a40099f050f52684
wjmmk <wjmmk3@gmail.com>
2026-04-21 16:03:07 -0500

    fix: Userschemas updated.

--- apps/ai-service/schemas.py
+++ apps/ai-service/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
 
 class BlogPost(BaseModel):
     id: int
@@ -19,15 +20,33 @@ class Item(BaseModel):
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=50)
 
 
 class PostCreate(PostBase):
-    pass
+    user_id: int # Temporary
 
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    user_id: int
     date_posted: str
+    author: UserResponse
+
+
+
+# Schemas of Final Users.
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    image_file: str | None
+    image_path: str
