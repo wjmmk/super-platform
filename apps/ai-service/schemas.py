3f047974ccd924ea6279eb452aed1e3e8216b73d
wjmmk <wjmmk3@gmail.com>
2026-04-22 13:04:23 -0500

    feat: UserUpdate schema created.

--- apps/ai-service/schemas.py
+++ apps/ai-service/schemas.py
@@ -42,13 +42,18 @@ class PostResponse(PostBase):
 
 
 
-# Schemas of Final Users.
+# Schemas of Users.
 class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     email: EmailStr = Field(max_length=120)
 
 class UserCreate(UserBase):
     pass
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 class UserResponse(UserBase):
     model_config = ConfigDict(from_attributes=True)
