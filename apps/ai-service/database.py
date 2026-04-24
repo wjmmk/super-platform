a41390bd04a793461796260444c937f492a278b1
wjmmk <wjmmk3@gmail.com>
2026-04-24 09:34:00 -0500

    feat: Update database configuration to use settings for PostgreSQL

--- apps/ai-service/database.py
+++ apps/ai-service/database.py
@@ -1,12 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
+from config import settings
+
+""" # SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
-)
+) """
+
+engine = create_async_engine(settings.database_url)
 
 # Esta linea crea Sessiones para las Transacciones.
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
